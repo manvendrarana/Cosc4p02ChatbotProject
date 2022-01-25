ccd1f9f99c81399ea6491df7f271f9f1c170ecc4
sk15xm@brocku.ca <sk15xm@brocku.ca>
2022-01-25 11:50:46 +0800

    alternative to xpath

--- Chatbot/scraping/test.js
+++ Chatbot/scraping/test.js
@@ -1,1 +1,40 @@
-/* test */
+const webdriver = require("selenium-webdriver");
+const until = webdriver.until;
+const By = webdriver.By;
+var goldNum = 0;
+
+const driver = new webdriver.Builder().forBrowser("chrome").build();
+
+let parseURL = async(url) =>
+{
+    await driver.get(url);
+    let element = await driver.findElement(By.className('ResultEventContainer'));
+    await driver.wait(until.elementIsVisible(element), 15000)
+    .then(() =>
+    {
+        const array = driver.findElements(By.className('ResultEventContainer'));
+        array.then((ele) =>
+        {
+            ele.forEach((arrayItem) =>
+            {
+                const nextElement = arrayItem.findElement(By.className('ResultEventHeaderContainer'));
+                nextElement.then((item) =>
+                {
+                    var goldNum = 0;
+                    item.getText().then((result) =>
+                    {
+                        if(result.includes("gold") || result.includes("Gold"))
+                        {
+                            console.log(result)
+                            goldNum++;
+                        }
+                    });
+                console.log("Number of gold medals: " + goldNum);
+                });
+            });
+
+        });
+    });
+}
+
+parseURL("https://cg2015.gems.pro/Result/ShowPerson.aspx?Person_GUID=7F1079FC-AF27-4858-82AB-9ECCBD675D7E&SetLanguage=en-CA")
