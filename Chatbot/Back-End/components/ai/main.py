95ce210d8a6846b1b22e6b81b660435d0bb5a1c6
Capt_KillSwitch <rh18vo@gmail.com>
2022-03-09 18:40:40 -0500

    updating info

--- Chatbot/Back-End/components/ai/main.py
+++ Chatbot/Back-End/components/ai/main.py
@@ -2,20 +2,21 @@
 # 
 
 
-from transformers import pipeline
-import pandas as pd
+# from traceback import print_tb
+# from transformers import pipeline
+# import pandas as pd
 
-print("Initializing Pipeline")
-tqa = pipeline(task="table-question-answering",
-               model="google/tapas-base-finetuned-wtq")
+# #if input() == "start":
+# tqa = pipeline(task="table-question-answering",
+#             model="google/tapas-base-finetuned-wtq")
 
-print("Loading Data")
-table = pd.read_csv("Athletes.csv")
-table = table.astype(str)
-print(table)
+# table = pd.read_csv("Topics.csv")
+# table = table.astype(str)
+# print("ready")
+# while True:
+#     query = input()
+#     result = tqa(table=table, query=query)
+#     print(result)
 
-while True:
-    print("Asking a question")
-    query = input()
-    result = tqa(table=table, query=query)
-    print(result)
+#pickle for storing data
+
